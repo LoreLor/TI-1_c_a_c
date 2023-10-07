be844b77b349fdab772b2253fe70ac5062ec7463
Lorena De Armas <lorenadearmas01@gmail.com>
2023-10-07 20:40:54 -0300

    mensaje-enviado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
 function navigateTicketek (){
     window.open("https://www.ticketek.com.ar", "_blank");
-}
+};
+
+const form = document.getElementById("messageForm");
+const confirmatinMesagge = document.getElementById("confirmationMessage");
+
+form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    confirmatinMesagge.style.display = "flex";
+
+    const name = document.getElementById("name");
+    const lastName  = document.getElementById("lastName");
+    const message = document.getElementById("message");
+
+    name.value = "";
+    lastName.value = "";
+    message.value = "";
+});
